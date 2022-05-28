3077d989fd453df5ae4471552bd925f20797669e
JKierby <jankierbygirang@gmail.com>
2022-05-28 20:29:06 +0800

    new 404

--- 404.php
+++ 404.php
@@ -1,1 +1,48 @@
-404 not found :(
+<!--
+
+    Work in Progress | Template by Nicolas Honrade
+
+-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>404: Page Not Found</title>
+    <!-- Website Icon -->
+    <link rel="icon" href="resources/img/logo-sm-trans.png">
+    
+    <!-- Bootstrap and Font Style -->
+        <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <!-- Bootstrap JS with Popper -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <!-- Google Font/Icons -->
+        <!-- Inter Font Style -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter"/>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
+
+    <!-- Custom CSS -->
+    <link href="resources/css/style.css" rel="stylesheet"/>
+    <link href="resources/css/themes/default.css" rel="stylesheet"/>
+
+</head>
+<body>
+    
+<canvas class="container-fluid min-vh-100" id="dots_bg"></canvas>
+<div class="container-fluid vh-100 p-0">
+    <div class="d-flex h-100 align-items-center justify-content-center text-center">
+        <h1 class="display-1 fw-bold text-white">404 <br/><span class="display-2">Page Not Found</span></h1>
+    </div>
+</div>
+
+
+</body>
+    <!-- PLUGINS -->
+        <!-- Connecting Dots JS-->
+        <script src="resources/js/dots.js" type="text/javascript"></script>
+        <!-- JQuery -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+</html>
